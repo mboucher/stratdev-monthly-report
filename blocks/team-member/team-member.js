fdf4413160109b56393b4aabca696229f98fbff2
Marcel Boucher <mboucher@adobe.com>
2024-01-22 14:42:09 -0400

    Styling updates

--- blocks/team-member/team-member.js
+++ blocks/team-member/team-member.js
@@ -1,3 +1,9 @@
-export default async function init() {
+/* eslint-disable no-plusplus */
+export default async function init(el) {
   // blockEl.classList.add('content');
+  const details = el.querySelectorAll('p');
+  details[0].classList.add('card-sub-title');
+  for (let i = 1; i < details.length; i++) {
+    details[i].classList.add('card-details');
+  }
 }
