ac65799a511cbd0ad6b4270ad0f359369a171c5a
Marcel Boucher <mboucher@adobe.com>
2024-02-20 15:25:13 -0500

    test

--- blocks/team-member/team-member.js
+++ blocks/team-member/team-member.js
@@ -3,7 +3,7 @@ export default async function init(el) {
   // blockEl.classList.add('content');
   const details = el.querySelectorAll('p');
   details[0].classList.add('card-sub-title');
-  for (let i = 1; i < details.length; i++) {
+  for (let i = 1; i < 3; i++) {
     details[i].classList.add('card-details');
   }
 }
