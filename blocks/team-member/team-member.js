f72741f0dc87ee57a864820d0b3a353400b8705f
Marcel Boucher <mboucher@adobe.com>
2024-02-20 17:21:07 -0500

    Add send email on card click

--- blocks/team-member/team-member.js
+++ blocks/team-member/team-member.js
@@ -1,9 +1,21 @@
+import { createTag } from '../../scripts/utils.js';
+
 /* eslint-disable no-plusplus */
 export default async function init(el) {
   // blockEl.classList.add('content');
   const details = el.querySelectorAll('p');
   details[0].classList.add('card-sub-title');
-  for (let i = 1; i < 3; i++) {
+  for (let i = 1; i < 2; i++) {
     details[i].classList.add('card-details');
+  }
+  const link = el.querySelectorAll('a');
+  if (link.length > 0) {
+    const element = link[0];
+    const target = element.getAttribute('href');
+    element.remove();
+    el.addEventListener('click', () => {
+      // eslint-disable-next-line no-restricted-globals
+      location.href = `${target}?Subject=Request for information from team website`;
+    });
   }
 }
